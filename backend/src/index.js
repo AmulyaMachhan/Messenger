bd65c1be5f97887ef176458f2e1bb6e0b0449a53
AmulyaMachhan <machhanamulya@gmail.com>
2024-10-21 05:10:13 +0530

    Updated routes in index

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+//Import routes
+import authRouter from "./routes/auth.routes.js";
+
+//Routes declaration
+app.use("api/v1/auth", authRouter);
+
 app.listen(5000, () => {
   console.log(`SERVER SUCCESSFULLY RUNNING ON PORT 5000`);
 });
