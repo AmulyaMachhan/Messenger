11519062538d6323d7ca96a6af43785588506da8
AmulyaMachhan <machhanamulya@gmail.com>
2024-10-22 18:09:13 +0530

    Updated auth router

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,4 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
+router.post("/signup", (req, res) => {
+  res.send("User sign up");
+});
+
+router.get("/login", (req, res) => {
+  res.send("User Login");
+});
+
 export default router;
