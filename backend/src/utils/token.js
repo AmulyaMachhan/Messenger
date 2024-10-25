25747067015d1a1c8023bf126af680eb96de0537
AmulyaMachhan <machhanamulya@gmail.com>
2024-10-25 23:09:14 +0530

    Created a utility for generating token

---
+++ backend/src/utils/token.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = async (userId, res) => {
+  try {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    res.cookie("jwt", {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+    });
+  } catch (error) {
+    console.error("Error in creating token" + error);
+  }
+};
