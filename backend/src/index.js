25d9a646716121b1cc1033c4bdbb9bd45ba7d5d2
AmulyaMachhan <machhanamulya@gmail.com>
2024-10-27 09:09:45 +0530

    Added middlewares to index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connection.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 //Import routes
 import authRouter from "./routes/auth.routes.js";
