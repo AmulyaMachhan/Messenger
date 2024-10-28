5a997d7a2535aa2b886605b303ac18eadabaafdf
AmulyaMachhan <machhanamulya@gmail.com>
2024-10-28 13:24:08 +0530

    Updated auth routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,7 +3,7 @@ import { signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
+router.route("/signup").post(signup);
 
 router.get("/login", (req, res) => {
   res.send("User Login");
