333182bf7b901f8c22f1788d8f3755ede99341da
AmulyaMachhan <machhanamulya@gmail.com>
2024-10-29 08:23:54 +0530

    Created auth middleware for token verification

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler";
+import { User } from "../models/user.model";
+
+const authentication = asyncHandler(async (req, res, next) => {
+  //Extract token from cookies using request
+  //Verify if that token is of a user
+  //Extract user data from the verified jwt
+  //Find if that user exists
+  //Verify that user
+  //Embed that user in req
+  //Use next flag for middleware
+
+  let token = req.cookies?.jwt;
+
+  if (!token) {
+    res.status(400).json({ message: "Unauthorized Access" });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decodedToken.userID).select("-password");
+
+    if (!user) {
+      res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized access" });
+    throw new Error("Unauthorized access");
+  }
+});
