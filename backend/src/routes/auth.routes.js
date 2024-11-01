1df330ee8f7509a6e8a0473a8c9c92e6b3bca5c1
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-01 13:25:08 +0530

    Created auth routes for updating profile

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  signup,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,4 +14,6 @@ router.route("/signup").post(signup);
 router.route("/login").post(login);
 
 router.route("/logout").get(logout);
+
+router.route("/update-profile").post(authenticate, updateProfile);
 export default router;
