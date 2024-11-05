e3335dc92369a464b6a5804331bce9e1003498f8
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-05 10:34:52 +0530

    Created a socket connection

--- backend/src/socket/socket.connection.js
+++ backend/src/socket/socket.connection.js
@@ -9,8 +9,33 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on("connection", (client) => {
-  console.log(client);
+//Object to map userId to SocketId
+const userToSocketId = {};
+
+//Function to get receiver socket Id through the receiver's user Id
+export const getReceiverSocketId = function (userId) {
+  return userToSocketId[userId];
+};
+
+//On client connection on the server
+io.on("connection", (socket) => {
+  console.log("A user has connected " + socket.id);
+
+  //Get userID fro the handshake query parameters from the frontend
+  const userId = socket.handshake.query.userId;
+  //Map that userId to the socketId
+  if (userId) userToSocketId[userId] = socket.id;
+
+  //Emit the online users to all the connected clients
+  io.emit("getOnlineUsers", Object.keys(userToSocketId));
+
+  //Listen to when a client listens to disconnect event event
+  socket.on("disconnect", () => {
+    console.log("A user has disconnected " + socket.id);
+    //Delete the use
+    delete userToSocketId[userId];
+    io.emit("getOnlineUsers", Object.keys(userToSocketId));
+  });
 });
 
-export { io, app, dotenv };
+export { io, app, server };
