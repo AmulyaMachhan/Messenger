967f75f122eb47054b927e30f189c307d1a030b3
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-06 23:56:08 +0530

    Removed bugs

--- backend/src/utils/token.js
+++ backend/src/utils/token.js
@@ -6,7 +6,7 @@ export const generateToken = async (userId, res) => {
       expiresIn: "30d",
     });
 
-    res.cookie("jwt", {
+    res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
       sameSite: "strict",
