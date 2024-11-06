5b4ce45ce6765de6becf9881600b2b6bbaa6b68f
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-06 14:13:23 +0530

    Created a route for sending messages to a user

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,5 +1,9 @@
 import express from "express";
+import { sendMessage } from "../controllers/message.controller.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.router();
+
+router.route("/send-message").post(authenticate, sendMessage);
 
 export default router;
