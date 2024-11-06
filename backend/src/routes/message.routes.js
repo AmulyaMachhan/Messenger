2218b412c336f59514775b85cc7d336702769989
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-06 19:04:22 +0530

    Created route for finding users

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getMessages, sendMessage } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.router();
 
 router.route("/send-message").post(authenticate, sendMessage);
 router.route("/get-messages").post(authenticate, getMessages);
+router.route("/users").post(authenticate, getUsersForSidebar);
 export default router;
