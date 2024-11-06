1d7e6c829f59006086948c6c1eed0411c91ef0f0
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-06 15:28:45 +0530

    Created a controller for getting messages between 2 users

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -4,6 +4,25 @@ import { getReceiverSocketId, io } from "../socket/socket.connection";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import cloudinary from "../utils/cloudinary.js";
 
+export const getMessages = asyncHandler(async (req, res) => {
+  //Extract senderId from middleware
+  const myId = req.user._id;
+
+  //Get receiverId
+  const { id: chatByUserId } = req.params;
+
+  //Find messages for that particular sender and receiverId
+  const messages = await User.find({
+    $or: [
+      { senderId: myId, receiverId: chatByUserId },
+      { senderId: chatByUserId, receiverId: myId },
+    ],
+  });
+
+  //Send the response
+  res.status(201).json(messages);
+});
+
 export const sendMessage = asyncHandler(async (req, res) => {
   //Extract senderId from middleware
   const senderId = req.user._id;
