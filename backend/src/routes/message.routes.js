942911ec4179a26b70b248754016a11c4edb0784
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-11 14:22:56 +0530

    Complete backend

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -8,8 +8,8 @@ import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.route("/send/:id").post(authenticate, sendMessage);
-router.route("/:id").get(authenticate, getMessages);
 router.route("/users").get(authenticate, getUsersForSidebar);
+router.route("/get/:id").get(authenticate, getMessages);
+router.route("/send/:id").post(authenticate, sendMessage);
 
 export default router;
