a60b0849a4f8f8a2a047ae85237a2c65c79b19da
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-12 15:23:45 +0530

    Updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import Navbar from "./components/skeletons/Navbar";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
     </>
   );
 }
