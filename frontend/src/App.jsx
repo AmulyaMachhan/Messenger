139340d1577f5fb49d25c94da029bf23e8e32077
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-13 11:21:36 +0530

    Validating if daisyui working fine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/skeletons/Navbar";
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 
