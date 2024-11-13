86dceea7221a3988eb281ff1376e22d0b3d1e78b
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-13 15:06:31 +0530

    Created a reducer for signing up

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { api } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -10,13 +11,28 @@ export const useAuthStore = create((set, get) => ({
 
   checkAuth: async () => {
     try {
-      const res = await api.get("/check-auth");
+      const res = await api.get("auth/check-auth");
       set({ authUser: res.data });
     } catch (error) {
       console.error("Error while checking user authentication " + error);
       set({ authUser: null });
     } finally {
       set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await api.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+    } catch (error) {
+      console.log("Error while registering a user " + error);
+      toast.error(error.response.data.message);
+      set({ authUser: null });
+    } finally {
+      set({ isSigningUp: false });
     }
   },
 }));
