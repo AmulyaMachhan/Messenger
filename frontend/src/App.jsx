74e1b5bb8b927375abc52a9355698683360f1604
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-17 14:38:09 +0530

    Update app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,36 @@
-import Navbar from "./components/skeletons/Navbar";
-import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import SignUpPage from "./pages/SignUpPage";
-import { useAuthStore } from "./store/useAuthStore";
-import { useEffect } from "react";
 import LoginPage from "./pages/LoginPage";
-import SettingsPage from "./pages/SettingsPage";
+import SignUpPage from "./pages/SignUpPage";
 import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import Navbar from "./components/skeletons/Navbar";
+
+import { useEffect } from "react";
+import { useAuthStore } from "./store/useAuthStore";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import { Toaster } from "react-hot-toast";
+import { Loader } from "lucide-react";
 
 function App() {
-  const { authUser, checkAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex item-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <>
       <Navbar />
+
       <Routes>
         <Route
           path="/"
@@ -37,6 +50,8 @@ function App() {
         />
         <Route path="/settings" element={<SettingsPage />} />
       </Routes>
+
+      <Toaster />
     </>
   );
 }
