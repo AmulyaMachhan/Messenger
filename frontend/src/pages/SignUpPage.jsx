25b7190e7f282614eb2f1bf00cbc16e32a504397
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-20 15:34:19 +0530

    Created signup functionality

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,55 @@
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+
 function SignUpPage() {
-  return <div>SignUp Page</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    password: "",
+    email: "",
+    fullName: "",
+  });
+
+  const { signup } = useAuthStore();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await signup(formData);
+  };
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <input
+          value={formData.fullName}
+          onChange={(e) =>
+            setFormData({ ...formData, fullName: e.target.value })
+          }
+        />
+        <input
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        />
+
+        <input
+          value={formData.password}
+          type={showPassword ? "text" : "password"}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+        ></input>
+        <button
+          onClick={() => {
+            setShowPassword(!showPassword);
+          }}
+        >
+          X
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUpPage;
