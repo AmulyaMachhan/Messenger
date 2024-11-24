5bf9f90e496f27cd376bb61aed6badce01e6847c
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-24 10:23:45 +0530

    Updated index with json limit extended

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,8 @@ import { app, server } from "./socket/socket.connection.js";
 
 dotenv.config();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
