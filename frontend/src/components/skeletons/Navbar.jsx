dd9ae484cef2085cae42e92dfaf0d1b6d2e6dfc6
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-24 12:23:11 +0530

    Created navbar functionality

--- frontend/src/components/skeletons/Navbar.jsx
+++ frontend/src/components/skeletons/Navbar.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router";
+import { useAuthStore } from "../../store/useAuthStore";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  const { logout, authUser } = useAuthStore();
+
+  const handleClick = () => {
+    logout();
+  };
+  return (
+    <div className="flex flex-end">
+      <div className="flex items-center gap-4">
+        {!authUser ? (
+          <div>
+            <button onClick={() => handleClick()}>Logout</button>
+          </div>
+        ) : (
+          <Link to="/profile">Profile</Link>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
