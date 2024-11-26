991bc0e41a5bdeb3756c7303756f1356f44dfb36
AmulyaMachhan <machhanamulya@gmail.com>
2024-11-26 09:43:17 +0530

    Created profile page functionality

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+
 const ProfilePage = () => {
-  return <div>Profile Page</div>;
+  const { authUser, updateProfile, isUpdatingProfile } = useAuthStore();
+
+  const [selectedImg, setSelectedImg] = useState(null);
+
+  const handleUpload = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = async () => {
+      const base64Image = reader.result;
+      setSelectedImg(base64Image);
+      await updateProfile({ picture: base64Image });
+    };
+  };
+  return (
+    <div className="p-12 mt-10">
+      <img
+        src={selectedImg || authUser.profilePic || ".../public/vite.svg"}
+        alt=""
+      />
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => {
+          handleUpload(e);
+        }}
+      />
+    </div>
+  );
 };
 
 export default ProfilePage;
