09998ae39afdf394cdc9d7df7c5f83669f2a767c
AmulyaMachhan <machhanamulya@gmail.com>
2024-12-03 11:19:54 +0530

    Created zustand store for themes

---
+++ frontend/src/store/useThemeStore.js
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+export const useThemeStore = create((set) => ({
+  theme: localStorage.getItem("chat-item") || "coffee",
+
+  setTheme: (theme) => {
+    localStorage.setItem("chat-item", theme);
+    set({ theme });
+  },
+}));
