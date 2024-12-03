67d4709c5412ac9a3133e1b8419eb6052f70c8ef
AmulyaMachhan <machhanamulya@gmail.com>
2024-12-03 13:57:26 +0530

    Added settings page for changing themes

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,5 +1,20 @@
+import { THEMES } from "../constants/THEMES";
+import { useThemeStore } from "../store/useThemeStore";
+
 const SettingsPage = () => {
-  return <div>Settings Page</div>;
+  const { theme, setTheme } = useThemeStore();
+
+  return (
+    <div className="flex flex-col">
+      {THEMES.map((t) => (
+        <button key={t} onClick={() => setTheme(t)}>
+          <span>{t}</span>
+        </button>
+      ))}
+
+      <div>{theme}</div>
+    </div>
+  );
 };
 
 export default SettingsPage;
